091e56c563dd92a18f99dbd17f21f854494894d6
MIZUNASHI, Rin <mizunashi.rin@gmail.com>
2018-01-04 12:06:25 +0900

    modified ja.js

--- src/languages/ja.js
+++ src/languages/ja.js
@@ -1,21 +1,21 @@
 module.exports = {
-    TITLE_OFFLINE_WARNING: "Use on an offline computer",
-    CONTENT_OFFLINE_WARNING: "If you haven't already, download this page and use it on an offline computer to keep your private keys secure!",
+    TITLE_OFFLINE_WARNING: "利用時はコンピューターをネットワークから切断してください。",
+    CONTENT_OFFLINE_WARNING: "まだダウンロードしていない場合には、このページをダウンロードして、オフラインコンピュータで利用する事で秘密鍵を安全に保ちます。",
 
-    LABEL_APP_LANGUAGE: "Language",
+    LABEL_APP_LANGUAGE: "言語",
 
-    TITLE_RECOVER_WALLET: "Enter mnemonic to recover NEM accounts",
-    LABEL_NETWORK: "Network",
-    LABEL_BIP39_MNEMONIC: "BIP-39 mnemonic",
-    LABEL_BIP39_PASSPHRASE: "BIP-39 passphrase",
+    TITLE_RECOVER_WALLET: "リカバリーが必要なNEMアカウントのニーモニックを入力して下さい。",
+    LABEL_NETWORK: "ネットワーク",
+    LABEL_BIP39_MNEMONIC: "BIP-39 ニーモニック",
+    LABEL_BIP39_PASSPHRASE: "BIP-39 パスフェース",
 
-    TITLE_DOWNLOAD_WALLET: "Enter a wallet name and password to export for NanoWallet",
-    LABEL_WALLET_NAME: "Wallet name",
-    LABEL_WALLET_PASSWORD: "Wallet password",
-    BUTTON_DOWNLOAD_WALLET: "Download wallet",
+    TITLE_DOWNLOAD_WALLET: "NanoWalletで利用するwltファイルを作成する為に、ウォレット名とパスワードを入力して下さい。",
+    LABEL_WALLET_NAME: "ウォレット名",
+    LABEL_WALLET_PASSWORD: "ウォレットパスワード",
+    BUTTON_DOWNLOAD_WALLET: "ウォレットのダウンロード",
 
-    TITLE_GENERATED_TABLE: "Generated accounts",
+    TITLE_GENERATED_TABLE: "復元されたアカウント",
     COLUMN_GENERATED_BIP32: "BIP-32",
-    COLUMN_GENERATED_ADDRESS: "Address",
-    COLUMN_GENERATED_PRIVATE_KEY: "Private key",
+    COLUMN_GENERATED_ADDRESS: "アドレス",
+    COLUMN_GENERATED_PRIVATE_KEY: "プライベートキー",
 };
